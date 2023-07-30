fb34519fd9cad23617bb9468a3d6a7756a96a6b9
opielapatryk <xpatrykopiela@gmail.com>
2023-07-30 11:31:48 +0200

    scrape table

--- clock/real_time_clock/views.py
+++ clock/real_time_clock/views.py
@@ -1,6 +1,13 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-
+import requests
+from bs4 import BeautifulSoup as bs
+import pandas as pd
 
 def index(req):
-    return render(req, 'real_time_clock.html', {})
+    url = "https://en.wikipedia.org/wiki/List_of_tz_database_time_zones"
+    r = requests.get(url)
+    soup = bs(r.content, 'html.parser')
+    table = soup.select('table')[0]
+    df = pd.read_html(str(table))[0]
+    rows = df.values.tolist()
+    return render(req, 'real_time_clock.html', context={'rows': rows})
